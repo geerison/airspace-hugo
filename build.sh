35b61bd9ff15549e87b4476947cec004a113eaa2
geerison <geerison@gmail.com>
2020-01-11 03:08:58 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf ./hugo_0.62.2_linux_amd64
 
 curl -L https://github.com/spf13/hugo/releases/download/v0.62.2/hugo_0.62.2_Linux-64bit.tar.gz | tar zxf -
 
-
+./hugo
 cd exampleSite
 hugo version
 hugo
