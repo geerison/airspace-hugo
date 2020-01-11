b9becae4c886ab3c49d78ae660122dafe7c1d136
geerison <geerison@gmail.com>
2020-01-11 03:21:23 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ rm -rf ./hugo_0.62.2_linux_amd64
 curl -L https://github.com/spf13/hugo/releases/download/v0.62.2/hugo_0.62.2_Linux-64bit.tar.gz | tar zxf -
 
 ./hugo
-
+hugo mod init ugly-dummy
 hugo version
 hugo
 
