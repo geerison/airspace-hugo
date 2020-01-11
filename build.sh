ca3f05fb06748de19c9a5e62e67ef2531c34e01f
geerison <geerison@gmail.com>
2020-01-11 03:06:29 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@ rm -rf ./hugo_0.62.2_linux_amd64
 
 curl -L https://github.com/spf13/hugo/releases/download/v0.62.2/hugo_0.62.2_Linux-64bit.tar.gz | tar zxf -
 
-HUGO=hugo
+
 cd exampleSite
-$HUGO version
-$HUGO
+hugo version
+hugo
 
 rsync -av -e 'ssh -p 22' ~/clone/public/ c14838admin@wh09.rackhost.hu: /public
 
